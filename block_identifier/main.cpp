caece75f1e7805a745780f2181d5285d1fcb4b71
mikuriya <mikuriya@mikuriya-no-MacBook-Pro.local>
2017-09-29 17:23:00 +0900

    find block contour

--- block_identifier/main.cpp
+++ block_identifier/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <cassert>
 
 struct Color
 {
@@ -41,6 +42,40 @@ std::string getColor(cv::Vec3b rgb)
     return dst;
 }
 
+/*!
+ カメラの画像からブロックの輪郭を抽出する
+ @param[in] m カメラ画像(RGB)
+ @return ブロックの輪郭
+ */
+std::vector<cv::Point> getBlockContour(cv::Mat const & m)
+{
+    cv::Mat hls;
+    cv::cvtColor(m, hls, CV_BGR2HLS);
+    std::vector<cv::Mat> v;
+    cv::split(hls, v);
+    auto l = v[1];
+    auto s = v[2];
+    double r = 0.5;
+    auto mixed = r * l + (1 - r) * s;
+    cv::Mat block;
+    cv::threshold(mixed, block, 90, 255, cv::THRESH_BINARY);
+    cv::imshow("block", block);
+    typedef std::vector<cv::Point> contour_t;
+    std::vector<contour_t> contours;
+    cv::findContours(block, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    // 一番面積の広い領域がブロックと判断する。ただし画像サイズなみの面積だった場合は除外
+    contour_t cach;
+    double maxArea = -1;
+    for(auto points:contours){
+        double area = cv::contourArea(points);
+        if(maxArea < area && area < m.cols * m.rows * 0.9){
+            maxArea = area;
+            cach = points;
+        }
+    }
+    return cach;
+}
+
 int main(int argc, const char * argv[]) {
     cv::VideoCapture cap(1);
     if(!cap.isOpened()){
@@ -57,30 +92,13 @@ int main(int argc, const char * argv[]) {
         cv::resize(m, m, cv::Size(), r, r);
         cv::flip(m.t(), m, 0);
         cv::imshow("src", m);
-        {
-            cv::Mat hls;
-            cv::cvtColor(m, hls, CV_BGR2HLS);
-            std::vector<cv::Mat> v;
-            cv::split(hls, v);
-            auto l = v[1];
-            auto s = v[2];
-            auto res = 0.5 * l + 0.5 * s;
-            cv::imshow("block", res);
-            cv::Mat bin;
-            cv::threshold(res, bin, 90, 255, cv::THRESH_BINARY);
-            cv::imshow("block + bin", bin);
-            std::vector<std::vector<cv::Point>> contours;
-            cv::findContours(bin, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-            bin = m.clone();
-            for(auto points:contours){
-                for(size_t i = 0; i < points.size(); ++i){
-                    auto pt0 = points[i];
-                    auto pt1 = points[(i + 1) % points.size()];
-                    cv::line(bin, pt0, pt1, cv::Scalar(0, 255, 0), 4);
-                }
-            }
-            cv::imshow("contour", bin);
+        auto blockContour = getBlockContour(m);
+        for(size_t i = 0; i < blockContour.size(); ++i){
+            auto pt0 = blockContour[i];
+            auto pt1 = blockContour[(i + 1) % blockContour.size()];
+            cv::line(m, pt0, pt1, cv::Scalar(0, 255, 0), 4);
         }
+        cv::imshow("block contour", m);
         {
             std::cout << "[colors]" << std::endl;
             int x = m.cols / 2;
