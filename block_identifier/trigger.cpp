d93935d41286171982a181e74685f97cf10f8259
hiroshi <hiroshi@iMacHiroshi2.local>
2018-06-06 11:37:53 +0900

    add button trigger

--- block_identifier/trigger.cpp
+++ block_identifier/trigger.cpp
@@ -16,7 +16,48 @@ public:
     }
 };
 
+#ifdef ENABLE_RASPBERRY_PI_CAMERA
+#include <bcm2835.h>
+class ButtonTrigger : public Trigger
+{
+    int m_gpio;
+    int sampling(int count)
+    {
+        int c = 0;
+        for(int i = 0; i < count; ++i){
+            c += bcm2835_gpio_lev(m_gpio);
+        }
+        return c;
+    }
+public:
+    ButtonTrigger()
+    : m_gpio(24)
+    {
+        if(!bcm2835_init()){
+            std::cerr << "failed to init bcm2835" << std::endl;
+            exit(1);
+        }
+        bcm2835_gpio_fsel(m_gpio, BCM2835_GPIO_FSEL_INPT );
+        bcm2835_gpio_set_pud(m_gpio, BCM2835_GPIO_PUD_UP);
+    }
+    void wait()
+    {
+        int const count = 20;
+        while(sampling(count) != 0){
+            bcm2835_delay(1);
+        }
+        while(sampling(count) != count){
+            bcm2835_delay(1);
+        }
+    }
+};
+#endif // ENABLE_RASPBERRY_PI_CAMERA
+
 std::shared_ptr<Trigger> Trigger::create()
 {
+#ifdef ENABLE_RASPBERRY_PI_CAMERA
+    return std::make_shared<ButtonTrigger>();
+#else
     return std::make_shared<StdinTrigger>();
+#endif
 }
