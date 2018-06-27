950196e3ac333003afe2b07948f18d1701e55260
hiroshi <hiroshi@iMacHiroshi2.local>
2018-06-27 10:22:05 +0900

    mod

--- python/block.py
+++ python/block.py
@@ -7,13 +7,22 @@ if img is None or img.shape[0] is 0:
   print('failed to open image')
   quit()
 
-def fillDividedBlocks(bin):
+class BlockInfo:
+  def __init__(self):
+    self.color = [0, 0, 0] # ブロックの色
+    self.rc = [0, 0, 0, 0] # ブロックの矩形
+    self.color_area = [0, 0, 0, 0] # ブロック色判定領域
+    self.ave = [0, 0, 0] # 平均色
+    self.width = 0 # ブロック幅
+
+
+def fill_divided_blocks(bin):
   kernel = np.array([[1], [1], [1]], np.uint8)
   iterations = 5
   dil = cv2.dilate(bin, kernel, iterations)
   return cv2.erode(dil, kernel, iterations)
 
-def getMaximumContour(contours):
+def get_maximum_contour(contours):
   contour = contours[0]
   max_area = -1
   for i in range(0, len(contours)):
@@ -22,51 +31,52 @@ def getMaximumContour(contours):
       contour = contours[i]
   return contour
 
-def getBlockContour(img):
+def get_block_contour(img):
   hsv = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
   mixed = cv2.addWeighted(hsv[1], 0.6, hsv[2], 1.0, 0)
   _, block = cv2.threshold(mixed, 200, 255, cv2.THRESH_BINARY)
   cv2.imshow("block", block)
-  filled = fillDividedBlocks(block)
+  filled = fill_divided_blocks(block)
   _, contours, _ = cv2.findContours(filled, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   cv2.imshow("original", img)
   cv2.imshow("mixed", mixed)
   cv2.imshow("h", hsv[0])
   cv2.imshow("s", hsv[1])
   cv2.imshow("v", hsv[2])
-  return getMaximumContour(contours)
+  return get_maximum_contour(contours)
 
-def getFirstBorder(m, r):
+def get_first_border(m, r):
   for y in r:
     if(20 <= m[y][0]):
       return y
   return 0
 
-def getTopBottom(bin):
+def get_top_bottom(bin):
   m = cv2.reduce(bin, 1, cv2.REDUCE_AVG)
-  top = getFirstBorder(m, range(m.shape[0]))
-  bottom = getFirstBorder(m, reversed(range(m.shape[0])))
+  top = get_first_border(m, range(m.shape[0]))
+  bottom = get_first_border(m, reversed(range(m.shape[0])))
   return top, bottom
 
-def getUnitBlock(bin, y):
-  return [0, y, 0, 0] # TODO
+def get_unit_block(bin, y):
+  dst = BlockInfo()
+  dst.rc[1] = y
+  return dst
 
-def getBlockInfo(contour, img):
+def get_block_info(contour, img):
   bin = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
   cv2.drawContours(bin, [contour], 0, 255, -1)
   cv2.imshow("bin", bin)
-  top, bottom = getTopBottom(bin)
+  top, bottom = get_top_bottom(bin)
   blockCount = int((bottom - top + 20 / 2) / 20)
   if blockCount < 0:
     return
   dst = []
   for i in range(blockCount):
     y = (top * (blockCount - i) + bottom * i) / blockCount
-    dst.append(getUnitBlock(bin, y))
+    dst.append(get_unit_block(bin, y))
   return dst
 
-contour = getBlockContour(img)
-info = getBlockInfo(contour, img)
-print(info)
+contour = get_block_contour(img)
+info = get_block_info(contour, img)
 
 cv2.waitKey(0)
